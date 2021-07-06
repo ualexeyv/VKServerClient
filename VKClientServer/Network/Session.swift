26544e3816ab8db87dee558f747a9ca4d8f9d083
ualexeyv <ualexeyv@gmail.com>
2021-07-06 17:30:22 +0500

    create session.swift for singleton purposes

---
+++ VKClientServer/Network/Session.swift
@@ -0,0 +1,15 @@
+//
+//  Session.swift
+//  VKClientServer
+//
+//  Created by пользовтель on 06.07.2021.
+//
+
+import UIKit
+
+class Session {
+    static let shared = Session()
+    private init() {}
+    var token: String = ""
+    var userId: Int = 7831620
+}
